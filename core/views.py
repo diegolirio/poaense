495c4de63c1b47c62249437c9c4481c3bbc645d9
Diego Lirio <diegolirio.dl@gmail.com>
2013-10-10 14:29:51 -0300

    ...

--- core/views.py
+++ core/views.py
@@ -2,8 +2,17 @@
 from django.shortcuts import render_to_response
 from django.shortcuts import redirect
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[-1].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
 def classificacao(request):
-	return render_to_response('_base.html',{'template': 'classificacao.html',})	
+	ip = get_client_ip(request)
+	return render_to_response('_base.html',{'template': 'classificacao.html', 'ip': ip})	
 	
 def rodada_1(request):
 	return render_to_response('_base.html',{'template': 'rodada_1/rodada_1.html',})				
