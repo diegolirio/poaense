7ed92d7d14e5fd5a244f371ae937c61cdb1995fb
Diego Lirio <diegolirio.dl@gmail.com>
2013-10-10 10:10:50 -0300

    rodada_1.html

--- core/views.py
+++ core/views.py
@@ -5,5 +5,11 @@ from django.shortcuts import redirect
 def classificacao(request):
 	return render_to_response('_base.html',{'template': 'classificacao.html',})	
 	
-def jogo_1(request):
-	return render_to_response('_base.html',{'template': 'jogo_1.html',})			
+def rodada_1(request):
+	return render_to_response('_base.html',{'template': 'rodada_1/rodada_1.html',})				
+	
+def phone_jogo_1(request):
+	return render_to_response('_base.html',{'template': 'rodada_1/phone_jogo_1.html',})			
+
+def phone_jogo_2(request):
+	return render_to_response('_base.html',{'template': 'rodada_1/phone_jogo_2.html',})	
--- poaense/urls.py
+++ poaense/urls.py
@@ -9,7 +9,9 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'poaense.views.home', name='home'),
 	url(r'^classificacao/$', 'core.views.classificacao', name='classificacao'),
-	url(r'^jogo_1/$', 'core.views.jogo_1', name='jogo_1'),
+	url(r'^rodada_1/$', 'core.views.rodada_1', name='rodada_1'),
+	url(r'^rodada_1/phone_jogo_1/$', 'core.views.phone_jogo_1', name='phone_jogo_1'),
+	url(r'^rodada_1/phone_jogo_2/$', 'core.views.phone_jogo_2', name='phone_jogo_2'),
     # url(r'^poaense/', include('poaense.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
